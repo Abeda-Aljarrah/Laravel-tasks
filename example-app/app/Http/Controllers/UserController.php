d63b7a1bf672ee702ad646443eab8abaf18966e0
abeda jarrah <Abeda-Aljarrah>
2023-08-29 17:04:07 +0300

    laravel-project

---
+++ example-app/app/Http/Controllers/UserController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $username = $request->input('email');
+        $password = $request->input('password');
+
+        $users = User::all();
+        $username1 = false;
+        $password1 = false;
+        foreach($users as $user){
+            if($user['email'] == $username){
+                $username1=true;
+            }
+        }    
+        if($username1){
+            foreach($users as $user){
+                if($user['password'] == $password){
+                    $password1 =true;
+                }
+            }
+        }
+
+    
+    if($username1 && $password1){
+        return view('index');
+    }else{
+        echo '<div class="alert alert-danger" role="alert" style="text-align: center;">wrong password</div>';
+        return view('login');
+    }
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
